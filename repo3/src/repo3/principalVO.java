8fe0f692e57c7ab39f21b6c8f2d962d18550a6e5
mamglezcom <contacto@mamglez.com>
2019-10-11 13:45:47 +0200

    subir vo

---
+++ repo3/src/repo3/principalVO.java
@@ -0,0 +1,21 @@
+package repo3;
+
+public class principalVO {
+	
+	String nombre;
+	
+	public void principalVO() {
+		
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+
+}
